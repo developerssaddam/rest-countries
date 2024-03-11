00cf7b81800e797d390672af935739546e562e36
developersaddam <developerssaddam@gmail.com>
2024-03-11 17:52:45 +0600

    Added onclick event on add visited country list

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,7 @@ import Country from "../Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountry, setVisitedCountry] = useState([]);
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/all`)
@@ -11,15 +12,55 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
 
-  console.log(countries);
+  // handleVisitedCountry.
+  const handleVisitedCountry = (country) => {
+    // console.log(country);
+    // const countryName = country.name.common;
+    // const countryFlag = country.flags.png;
+    // if (!visitedCountry.includes(countryName)) {
+    //   const newArr = [...visitedCountry, countryName];
+    //   setVisitedCountry(newArr);
+    // } else {
+    //   const newArr = visitedCountry.filter((item) => item !== countryName);
+    //   setVisitedCountry(newArr);
+    // }
+
+    if (!visitedCountry.includes(country)) {
+      const newArr = [...visitedCountry, country];
+      setVisitedCountry(newArr);
+    } else {
+      const newArr = visitedCountry.filter((item) => item !== country);
+      setVisitedCountry(newArr);
+    }
+  };
 
   return (
     <div>
       <h1>All-countries</h1>
+      <div>
+        <hr />
+        <h3 style={{ borderBottom: "2px solid red", display: "inline-block" }}>
+          Visited Country List
+        </h3>
+        <div className="listContainer">
+          <ol className="ol">
+            {visitedCountry.map((item) => (
+              <li key={item.name.common}>
+                <h3>Name : {item.name.common}</h3>
+                <img src={item.flags.png} alt="" />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
       <hr />
       <dir className="country_container">
         {countries.map((country) => (
-          <Country key={country.name.common} country={country}></Country>
+          <Country
+            key={country.name.common}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+          ></Country>
         ))}
       </dir>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
+  const [visited, setVisited] = useState(false);
+
+  // Handle visited btn.
+  const handleVisitedBtn = () => {
+    setVisited(!visited);
+  };
+
   const { name, cca3, flags, timezones } = country;
   return (
-    <div className="country_item">
+    <div className={visited ? "country_item_visited" : "country_item"}>
       <h4>Name : {name.common}</h4>
       <img src={flags.png} alt="" />
       <p>Time-Zone : {timezones[0]}</p>
       <p>CCA3 : {cca3}</p>
+      <button
+        onClick={() => {
+          handleVisitedBtn();
+          handleVisitedCountry(country);
+        }}
+        className={visited ? "visitedBtn" : ""}
+      >
+        {visited ? "Visited" : "Want to visit"}
+      </button>
     </div>
   );
 };
